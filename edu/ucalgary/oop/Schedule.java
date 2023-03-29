a2892c0b63c23e31e8cd36063254f57466931d87
“[Christy <[christy.guirguis@ucalgary.ca]>
2023-03-29 15:17:50 -0600

    init methods

--- edu/ucalgary/oop/Schedule.java
+++ edu/ucalgary/oop/Schedule.java
@@ -1,5 +1,31 @@
 package edu.ucalgary.oop;
+import java.util.Arrays;
 
 public class Schedule {
-    
+    private Schedule[][] scheduleArr;
+
+    public Schedule(Schedule [][] scheduleArr){
+        this.scheduleArr = scheduleArr;
+    }
+
+    public boolean backupVolunteer(){
+        boolean volunteer = false;
+        return volunteer;
+    }
+
+    public Animal[] getAnimals(){
+
+    }
+
+    public MedicalTask[] getMedical(){
+
+    }
+
+    public void printSchedule(){
+        for (int i = 0; i < scheduleArr.length; i++) {
+            System.out.println(Arrays.toString(scheduleArr[i]));
+        }
+    }
+
 }
+
