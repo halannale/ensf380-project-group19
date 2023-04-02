c157b8b99dd1079db750c484bdee1785353c7e45
[gracejang] <grace.jang1@ucalgary.ca>
2023-04-02 15:22:50 -0600

    drafts

---
+++ edu/ucalgary/oop/SchedulePrint.java
@@ -0,0 +1,25 @@
+package edu.ucalgary.oop;
+
+public class SchedulePrint {
+    public void printDetails(String[] args) {
+        if (backupVolunteer.exists()){
+            String time = assignTime.time() + backupVolunteer.exists();
+            for (int i = 0; i> assignTime.medicalTreatment(); i++){
+                String medTreatment= assignTime.medicalTreatment();
+            }
+            for (int i = 0; i> assignTime.feeding(); i++){
+                String feeding= assignTime.medicalTreatment();
+            }
+        }
+        else{
+            String time = assignTime.time();
+            for (int i = 0; i> assignTime.medicalTreatment(); i++){
+                String medTreatment= assignTime.medicalTreatment();
+            }
+            for (int i = 0; i> assignTime.feeding(); i++){
+                String feeding= assignTime.medicalTreatment();
+            }
+        }
+
+    }
+}
