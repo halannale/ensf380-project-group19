c2b942790a5345097c11758dd6359f163db125de
[gracejang] <grace.jang1@ucalgary.ca>
2023-04-03 00:03:10 -0600

    formatting of Schedule print is done

--- edu/ucalgary/oop/SchedulePrint.java
+++ edu/ucalgary/oop/SchedulePrint.java
@@ -3,26 +3,57 @@ package edu.ucalgary.oop;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.time.LocalDate;
+
 
 public class SchedulePrint {
-    private HashMap<Integer, ArrayList<String>> schedule;
     private AssignTime assignTime;
 
     public SchedulePrint(Animal[] animals, Treatment[] treatments) throws IllegalSchedule {
         AssignTime assignTime = new AssignTime(animals, treatments);
-        HashMap<Integer, ArrayList<String>> schedule = assignTime.getSchedule();
-        this.schedule = schedule;
+        
         this.assignTime = assignTime;
     }
 
     public void printSchedule() {
-        for (int i = 0; i < 24; i++) {
-            ArrayList<String> tasksInHour = schedule.get(i);
+        LocalDate currentDate = LocalDate.now();
+        String dateText = currentDate.toString();
+
+
+        int startTime = 0;
+        try{
+            FileWriter writer = new FileWriter("schedule.txt", true);
+            writer.write("Schedule for " + dateText + "\n");
+            writer.close();
+
+        }
+
+
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < 24; i++) { 
+            ArrayList<String> tasksInHour = assignTime.getSchedule().get(i);
+            int[] hour = assignTime.getAvailableTime();
+            int indexhour = hour[i];
+            if (indexhour == 120){
+                startTime = i;
+            }
             try {
                 FileWriter writer = new FileWriter("schedule.txt", true);
+                if(indexhour == 120){
+                    writer.write(startTime+":00" + " [+ backup volunteer]" +"\n");
+                }
+                else {
+                    if(tasksInHour.size() !=0 ){
+                        writer.write(i+":00" +"\n");
+                    }
+                }
+                
                 for (String task : tasksInHour) {
-                    writer.write(task + "\n");
+                    
+                    writer.write("* " + task + "\n");
                 }
                 writer.close();
             } catch (IOException e) {
@@ -31,10 +62,6 @@ public class SchedulePrint {
             }
         }
         System.out.println("Schedule written to schedule.txt");
-    }
-
-    public HashMap<Integer, ArrayList<String>> getSchedule() {
-        return this.schedule;
     }
 
     public AssignTime getAssignTime() {
