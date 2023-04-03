5cf5abbad231ef70901dcc7b61fbde4c784a5e92
halannale <halanna.le@ucalgary.ca>
2023-04-02 23:27:33 -0600

    gui updated to show what time backup is needed

--- edu/ucalgary/oop/ScheduleGUI.java
+++ edu/ucalgary/oop/ScheduleGUI.java
@@ -50,35 +50,29 @@ public class ScheduleGUI extends JFrame implements ActionListener, MouseListener
         tasks[2] = new MedicalTask(3, "Apply burn ointment back", 10, 3);
 
         Treatment[] treatments = new Treatment[5];
-        treatments[0] = new Treatment(animals[2], tasks[0],13);
-        treatments[1] = new Treatment(animals[5], tasks[0], 14);
-        treatments[2] = new Treatment(animals[5], tasks[0], 15);
-        treatments[3] = new Treatment(animals[1], tasks[0], 16);
-        treatments[4] = new Treatment(animals[4], tasks[0], 17);
+        treatments[0] = new Treatment(animals[2], tasks[1],0);
+        treatments[1] = new Treatment(animals[5], tasks[1], 0);
+        treatments[2] = new Treatment(animals[5], tasks[1], 0);
+        treatments[3] = new Treatment(animals[1], tasks[1], 0);
+        treatments[4] = new Treatment(animals[4], tasks[1], 0);
         try {
             SchedulePrint schedulePrint = new SchedulePrint(animals, treatments);
             schedulePrint.printSchedule();
-            int backup = 0;
+            int backup = -1;
             for (int i=0; i<24; i++) {
                 if (schedulePrint.getAssignTime().getAvailableTime()[i] == 120) {
-                    backup = 1;
+                    backup = i;
                 }
+                if (backup != -1) {
+                    confirmClose++;
+                    JOptionPane.showMessageDialog(this, String.format("Schedule created, backup volunteer needed at %1$d:00. Confirm they are called by pressing OK", backup));
+                    confirmClose--;
+                }
+                backup  = -1;
             }
-            if (backup == 1) {
-                confirmClose++;
-                JOptionPane.showMessageDialog(this, "Schedule created, backup volunteers needed. Please confirm they are called");
-                confirmClose--;
-
-                confirmClose++;
-                JOptionPane.showMessageDialog(this, "Schedule created");
-                confirmClose--;
-            }
-    
-            else {
-                confirmClose++;
-                JOptionPane.showMessageDialog(this, "Schedule created");
-                confirmClose--;
-            }
+            confirmClose++;
+            JOptionPane.showMessageDialog(this, "Schedule created");
+            confirmClose--;
         }
         catch (IllegalSchedule e) {
             //if illegal schedule
