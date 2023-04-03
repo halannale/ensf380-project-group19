e79f5b04146556d8e0021f9ff9a2f4e59d13961a
“[Christy <[christy.guirguis@ucalgary.ca]>
2023-04-03 14:25:49 -0600

    made objects array for treatments

--- edu/ucalgary/oop/SQLInfo.java
+++ edu/ucalgary/oop/SQLInfo.java
@@ -12,13 +12,19 @@ public class SQLInfo {
 
     private static ArrayList<String[]> animals = new ArrayList<String[]>();
     private static ArrayList<String[]> treatments = new ArrayList<String[]>();
-    private static ArrayList<String[]> animalsTreatment = new ArrayList<String[]>();
     private static ArrayList<String[]> tasks = new ArrayList<String[]>();
-    private Animal []  animals_obj;
+    private Animal [] animals_obj;
+    private Treatment [] treatment_obj;
+    private MedicalTask [] tasks_obj;
 
 
-    public Animal [] getAnimal(){
+
+    public Animal [] getAnimals(){
         return this.animals_obj;
+    }
+
+    public Treatment [] getTreatments(){
+        return this.treatment_obj;
     }
 
 
@@ -61,7 +67,6 @@ public class SQLInfo {
 
             treatments.clear();
 
-            
             while (results.next()) {
                 String[] treatmentData = new String[4];
                 treatmentData[0] = Integer.toString(results.getInt("TreatmentID"));
@@ -79,72 +84,27 @@ public class SQLInfo {
         return treatments;
     }
 
-    public ArrayList<String[]> animalTreatments() {
+    public ArrayList<String[]> selectTasks() {
         try {
             Statement myStmt = dbConnect.createStatement();
-            results = myStmt.executeQuery("SELECT * FROM TREATMENTS");
-            
+            results = myStmt.executeQuery("SELECT * FROM TASKS");
+
             while (results.next()) {
-                int animalID = results.getInt("AnimalID");
-                
-                for (String[] animalData : animals) {
-                    if (Integer.parseInt(animalData[0]) == animalID) {
-                        String[] animalTreatData = new String[3];
-                        animalTreatData[0] = animalData[0];
-                        animalTreatData[1] = animalData[1];
-                        animalTreatData[2] = animalData[2];
-                        animalsTreatment.add(animalTreatData);
-                        break;
-                    }
-                }
+                String[] taskData = new String[4];
+                taskData[0] = Integer.toString(results.getInt("TaskID"));
+                taskData[1] = results.getString("Description");
+                taskData[2] = results.getString("Duration");
+                taskData[3] = results.getString("MaxWindow");
+
+                tasks.add(taskData);
             }
-    
+
             myStmt.close();
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
-        return animalsTreatment;
+        return tasks;
     }
-
-    public ArrayList<String[]> tasks() {
-        ArrayList<String[]> tasksTreatment = new ArrayList<String[]>();
-        try {
-            Statement myStmt = dbConnect.createStatement();
-            results = myStmt.executeQuery("SELECT * FROM TREATMENTS");
-    
-            while (results.next()) {
-                int taskID = results.getInt("TaskID");
-    
-                PreparedStatement stmt = dbConnect.prepareStatement("SELECT * FROM TASKS WHERE TaskID = ?");
-                stmt.setInt(1, taskID);
-                ResultSet taskResults = stmt.executeQuery();
-    
-                while (taskResults.next()) {
-                    String[] taskData = new String[4];
-                    taskData[0] = taskResults.getString("TaskID");
-                    taskData[1] = taskResults.getString("Description");
-                    taskData[2] = taskResults.getString("Duration");
-                    taskData[3] = taskResults.getString("MaxWindow");
-                    tasksTreatment.add(taskData);
-                }
-            }
-    
-            myStmt.close();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        return tasksTreatment;
-    }
-    
-    public void close() {
-        try {
-            results.close();
-            dbConnect.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
 
     public Animal[] createAnimalsList() {
         int numAnimals = animals.size();
@@ -167,11 +127,53 @@ public class SQLInfo {
     
         return animals_obj;
     }
+
+    public MedicalTask[] createTasksList() {
+        int numTasks = tasks.size();
+        tasks_obj = new MedicalTask[numTasks];
     
+        for (int i = 0; i < numTasks; i++) {
+            String[] taskData = tasks.get(i);
+            int taskID = Integer.parseInt(taskData[0]);
+            String description = taskData[1];
+            int duration = Integer.parseInt(taskData[2]);
+            int maxWindow = Integer.parseInt(taskData[3]); 
+            MedicalTask new_task = new MedicalTask(taskID, description, duration, maxWindow);
+            tasks_obj[i] = new_task;
+        }
     
+        return tasks_obj;
+    }
+
+    public Treatment[] createTreatmentList() {
+        int numTreatments = treatments.size();
+        treatment_obj = new Treatment[numTreatments];
+
+        for (int i = 0; i < numTreatments; i++){
+            String[] treatmentData = treatments.get(i);
+            int animalIndex = Integer.parseInt(treatmentData[1]) - 1;
+            Animal animal = animals_obj[animalIndex];
+            int taskIndex = Integer.parseInt(treatmentData[2]) - 1;
+            MedicalTask task = tasks_obj[taskIndex];
+            int startHour = Integer.parseInt(treatmentData[3]);
+            
+            Treatment new_treatment = new Treatment(animal, task, startHour);
+            treatment_obj[i] = new_treatment;
+            
+        }
+        return treatment_obj;
+    }
+    
+    public void close() {
+        try {
+            results.close();
+            dbConnect.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
-        
  
         EventQueue.invokeLater(() -> {
             new ScheduleGUI().setVisible(true);        
@@ -186,24 +188,17 @@ public class SQLInfo {
         for (String[] animalData : animals) {
             System.out.println("AnimalID: " + animalData[0] + ", AnimalNickname: " + animalData[1] + ", AnimalSpecies: " + animalData[2]);
         }
-        
+      
         treatments = mySQL.selectTreatments();
         System.out.println("Here is a list of treatments: ");
         for (String[] treatmentData : treatments) {
             System.out.println("TreatmentID: " + treatmentData[0] + ", AnimalID: " + treatmentData[1] + ", TaskID: " + treatmentData[2] + ", StartHour: " + treatmentData[3]);
         }
 
-        animalsTreatment = mySQL.animalTreatments();
-        System.out.println("Here is a list of ANIMAL treatments: ");
-        for (String[] animalID : animalsTreatment) {
-            System.out.println("AnimalID: " + animalID[0] + ", AnimalNickname: " + animalID[1] + ", AnimalSpecies: " + animalID[2]);
-        }
-
-
-        tasks = mySQL.tasks();
+        tasks = mySQL.selectTasks();
         System.out.println("Here is a list of TASKS: ");
-        for (String[] taskID : tasks) {
-            System.out.println("taskID: " + taskID[0] + ", Description: " + taskID[1] + ", Duration: " + taskID[2]);
+        for (String[] taskData : tasks) {
+            System.out.println("taskID: " + taskData[0] + ", Description: " + taskData[1] + ", Duration: " + taskData[2] + ", MaxWindow" + taskData[3]);
         }
         
         SQLInfo obj = new SQLInfo();
@@ -214,6 +209,20 @@ public class SQLInfo {
         
         for (Animal animal : animalArray) {
             System.out.println(animal.getID() + ", " + animal.getName() + ", " + animal.getSpecies());
+        }
+
+        MedicalTask[] tasksArray = obj.createTasksList();
+
+        
+        for (MedicalTask task : tasksArray) {
+            System.out.println(task.getID() + ", " + task.getDescription() + ", " + task.getDuration() + ", " + task.getMaxWindow());
+        }
+
+        Treatment[] treatsArray = obj.createTreatmentList();
+
+        
+        for (Treatment treatment : treatsArray) {
+            System.out.println(treatment.getStartHour() + ", " + treatment.getAnimal().getID() + ", " + treatment.getMedical().getID());
         }
 
         mySQL.close();
